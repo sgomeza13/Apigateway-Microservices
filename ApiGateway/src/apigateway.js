3219b374dd582839996cb084f565689bd60acb42
sgomeza13 <sgomeza13@eafit.edu.co>
2023-08-27 16:22:17 -0500

    fxd

--- ApiGateway/src/apigateway.js
+++ ApiGateway/src/apigateway.js
@@ -37,29 +37,30 @@ app.get('/listfiles',(req, res)=>{
     client.SearchR({request_service:request_service},(err,data) => {
         if(err){
             //res.send(err)
-            amqp.connect('amqp://localhost', function(error0, connection) {
-            if (error0) {
-                throw error0;
-            }
-            connection.createChannel(function(error1, channel) {
-            if (error1) {
-                throw error1;
-            }
-            var queue = 'search';
-            var msg = 'Hello world';
-
-            channel.assertQueue(queue, {
-            durable: false
+            amqp.connect('amqp://simon:password@18.214.11.58:5672', function(error0, connection) {
+                if (error0) {
+                    throw error0;
+                }
+                connection.createChannel(function(error1, channel) {
+                    if (error1) {
+                        throw error1;
+                    }
+            
+                    var queue = 'hello';
+                    var msg = 'Hello World!';
+            
+                    channel.assertQueue(queue, {
+                        durable: false
+                    });
+                    channel.sendToQueue(queue, Buffer.from(msg));
+            
+                    console.log(" [x] Sent %s", msg);
+                });
+                setTimeout(function() {
+                    connection.close();
+                    process.exit(0);
+                }, 500);
             });
-
-            channel.sendToQueue(queue, Buffer.from(msg));
-            console.log(" [x] Sent %s", msg);
-            });
-        });
-        setTimeout(function() {
-            connection.close();
-            process.exit(0)
-            }, 500);
         }
         else{
             res.send(data)
