7a839c0056de99e6b453397cb7ed902f3c69945c
sgomeza13 <sgomeza13@eafit.edu.co>
2023-08-28 00:12:42 -0500

    .

--- ApiGateway/src/apigateway.js
+++ ApiGateway/src/apigateway.js
@@ -33,49 +33,8 @@ let file_search;
 const queue = "hello";
 
 app.get('/rabbit',(req,res)=>{
-    amqp.connect("amqp://simon:password@18.214.11.58:5672",  function(error0, connection) {
-        if (error0) {
-          throw error0;
-        }
-        connection.createChannel(function(error1, channel) {
-          if (error1) {
-            throw error1;
-          }
-          channel.assertQueue('', {
-            exclusive: true
-          }, function(error2, q) {
-            if (error2) {
-              throw error2;
-            }
-            var correlationId = generateUuid();
-            var num = 9;
-      
-            console.log(' [x] Requesting listfiles');
-      
-            channel.consume(q.queue, function(msg) {
-              if (msg.properties.correlationId == correlationId) {
-                console.log(' [.] Found %s', msg.content.toString());
-                setTimeout(function() {
-                  connection.close();
-                  //process.exit(0)
-                }, 500);
-                
-              }
-              
-            }, {
-              noAck: true
-            });
-      
-            channel.sendToQueue(queue,
-              Buffer.from(num.toString()),{
-                correlationId: correlationId,
-                replyTo: q.queue });
-          });
-        });
-        
-      });
-                 
-      res.send("msg");
+    somename();             
+    res.send("msg");
 });
 
 app.get('/listfiles',( req, res)=> {
@@ -114,8 +73,92 @@ app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
   });
 
+    amqp.connect("amqp://simon:password@18.214.11.58:5672",  function(error0, connection) {
+        if (error0) {
+          throw error0;
+        }
+        connection.createChannel(function(error1, channel) {
+          if (error1) {
+            throw error1;
+          }
+          channel.assertQueue('', {
+            exclusive: true
+          }, function(error2, q) {
+            if (error2) {
+              throw error2;
+            }
+            var correlationId = generateUuid();
+            var num = 9;
+      
+            console.log(' [x] Requesting listfiles');
+      
+            channel.consume(q.queue, function(msg) {
+              if (msg.properties.correlationId == correlationId) {
+                console.log(' [.] Found %s', msg.content.toString());
+                setTimeout(function() {
+                  connection.close();
+                }, 500);
+                
+              }
+              
+            }, {
+              noAck: true
+            });
+      
+            channel.sendToQueue(queue,
+              Buffer.from(num.toString()),{
+                correlationId: correlationId,
+                replyTo: q.queue });
+          });
+        });
+        
+      });
+
+
+      function somename(){
+        amqp.connect("amqp://simon:password@18.214.11.58:5672", function(error0, connection) {
+          if (error0) {
+            throw error0;
+          }
+          connection.createChannel(function(error1, channel) {
+            if (error1) {
+              throw error1;
+            }
+            channel.assertQueue('', {
+              exclusive: true
+            }, function(error2, q) {
+              if (error2) {
+                throw error2;
+              }
+              var correlationId = generateUuid();
+              var num = 9;
+        
+              console.log(' [x] Requesting listfiles');
+        
+              channel.consume(q.queue, function(msg) {
+                if (msg.properties.correlationId == correlationId) {
+                  console.log(' [.] Found %s', msg.content.toString());
+                  setTimeout(function() {
+                    connection.close();
+                    process.exit(0)
+                  }, 500);
+                }
+              }, {
+                noAck: true
+              });
+        
+              channel.sendToQueue(queue,
+                Buffer.from(num.toString()),{
+                  correlationId: correlationId,
+                  replyTo: q.queue });
+            });
+          });
+        });
+        }
+
 function generateUuid() {
     return Math.random().toString() +
             Math.random().toString() +
            Math.random().toString();
   }
+
