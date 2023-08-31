bb53d7bef7f4b5a486a55c69f5b34e0df2b5b6ff
sgomeza13 <sgomeza13@eafit.edu.co>
2023-08-30 22:12:52 -0500

    .

--- ApiGateway/src/apigateway.js
+++ ApiGateway/src/apigateway.js
@@ -110,7 +110,7 @@ app.post('/searchfile',(req, res)=>{
 app.get('/lostrequests', async(req,res)=>{
   let lostrequests;
   try{
-   await channel.assertQueue('cola_request_perdidos', {durable:false});
+   await channel.assertQueue('cola_request_perdidos', {durable:true,persistent:true});
    await channel.consume('cola_request_perdidos', (data) => {
     console.log(`Received ${Buffer.from(data.content)}`)
     const request = JSON.parse(`${Buffer.from(data.content)}`);
